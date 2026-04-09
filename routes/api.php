49c56041f3c5f45dfa3307b5824e4d433379901c
Juan Luis <juanluis@dropstudio.mx>
2026-04-09 11:04:20 -0600

    feat(auth): Definir rutas API para autenticación avanzada

--- routes/api.php
+++ routes/api.php
@@ -11,9 +11,18 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/forgot-password', [AuthController::class, 'forgotPassword'])->name('password.email');
+    Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.update');
+
+    Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])
+        ->middleware('signed')
+        ->name('verification.verify');
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/email/verification-notification', [AuthController::class, 'resendVerificationEmail'])
+            ->middleware('throttle:6,1')
+            ->name('verification.send');
 
         Route::apiResource('clients', ClientController::class);
         Route::apiResource('suppliers', SupplierController::class);
